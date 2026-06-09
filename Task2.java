52e1cfa5f9b6e124a12d7430270f0320d32a10c3
somarjp <juanpaolosomar@gmail.com>
2026-06-09 21:25:31 +0800

    Task2
    CIICC JAVA TASK 2

---
+++ Task2.java
@@ -0,0 +1,16 @@
+public class Task2{
+
+    public static void main (String [] args){
+        //H3110 w0r1d 2.0 true
+        //byte, short, int, long, float, double, boolean, char
+        int a = 311;
+        byte b = 0;
+        short c = 0;
+        char d = '1';
+        float e = 2.0f;
+        boolean f = true;
+
+        String output ="H"+a+b+" w"+c+"r"+d+"d "+e+" "+f;
+        System.out.println(output);
+    }
+}
